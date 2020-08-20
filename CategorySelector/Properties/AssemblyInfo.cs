d17654331c4a3afd8e87c17f7328c576ba0ebd06
pk5000 <pkaramitsos@visualantidote.com>
2020-08-20 09:16:30 -0400

    v1.0.0 for nuget

--- CategorySelector/Properties/AssemblyInfo.cs
+++ CategorySelector/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using CMS;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.7.2.0")]
-[assembly: AssemblyFileVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.7.12.0")]
+[assembly: AssemblyFileVersion("1.7.12.0")]
